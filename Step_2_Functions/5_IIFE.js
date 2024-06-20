35cfbb9c17d27cf02b04b26c2be6d9ded8efd955
KK's Repo <deltacodingketan@gmail.com>
2024-06-20 10:01:50 +0000

    IIFE Function

---
+++ Step_2_Functions/5_IIFE.js
@@ -0,0 +1,15 @@
+// Immediatly Invoked Function
+// Function Declaration
+
+// Named IIFE
+(function message() {
+  console.log("Hello All! Consistancy is key of the Success");
+})();
+
+// When you using IIFE Function You have to end with (semicolan/;) Always remamber
+
+// Simple IIFE
+// You also can pass the parameter to thr IIFE
+((name) => {
+  console.log(`Hello Team! My Name is ${name}`);
+})("Ketan");
