5b1190ca364b15a864dfa8b0dfc152be771e59c4
KK's Repo <deltacodingketan@gmail.com>
2024-06-25 10:48:41 +0000

    Filter Map and Reduce Simple Example

---
+++ Step_7_Filter_Map_Reduce/1_FMR.js
@@ -0,0 +1,25 @@
+// Array Declaration
+// const codingLang = ["js", "java", "ruby", "python", "c", "cpp", "golang"];
+// Using For Each Loop
+// const result = codingLang.forEach((data) => {
+//   return data;
+// });
+// console.log(result);
+
+// Number Array
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const newData = myNums.filter((num) => {
+  // You have to Return value with the return KEYWORD
+  // If your open the scope you Need to write the return keyword
+  // If you are not opening the scope you can written it directly
+  return num >= 5;
+});
+// console.log(newData);
+
+// Using the foreach Loop
+// myNums.forEach((data) => {
+//   if (data >= 5) {
+//     console.log(data);
+//   }
+// });
+// console.log(resultData);
