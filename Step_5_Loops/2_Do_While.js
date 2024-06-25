4575b8f6bf5dd48b7c2b022a566901a7d2eea95e
KK's Repo <deltacodingketan@gmail.com>
2024-06-25 06:47:24 +0000

    Simple Do While Loop

---
+++ Step_5_Loops/2_Do_While.js
@@ -0,0 +1,17 @@
+let i = 0;
+while (i <= 10) {
+  //console.log(i);
+  if (i == 6) {
+    // console.log("Detected 5");
+    break;
+  }
+  i += 2;
+}
+
+// Do While Loop
+let count = 1;
+do {
+  console.log(`Count is : ${count}`);
+  // Increment the Count
+  count++;
+} while (count <= 10);
